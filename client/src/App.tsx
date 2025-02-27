c1f3278d57c9d49e37eb33b313b8c4a6692dd974
berlin993 <hammadcoding@gmail.com>
2025-02-27 09:57:25 +0500

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "wouter";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { Toaster } from "@/components/ui/toaster";
+import { Navbar } from "@/components/layout/navbar";
+import Home from "@/pages/home";
+import Auth from "@/pages/auth";
+import Puzzle from "@/pages/puzzle";
+import Reward from "@/pages/reward";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/auth" component={Auth} />
+      <Route path="/puzzle" component={Puzzle} />
+      <Route path="/reward" component={Reward} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
+      <Router />
+      <Toaster />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
