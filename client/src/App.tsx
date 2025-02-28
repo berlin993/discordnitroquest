2d878b1f386164d57f700e6ac27ffc969e32da6b
berlin993 <hammadcoding@gmail.com>
2025-02-28 12:23:14 +0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { Navbar } from "@/components/layout/navbar";
 import Home from "@/pages/home";
-import Auth from "@/pages/auth";
 import Puzzle from "@/pages/puzzle";
 import Reward from "@/pages/reward";
 import NotFound from "@/pages/not-found";
@@ -13,7 +12,6 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/auth" component={Auth} />
       <Route path="/puzzle" component={Puzzle} />
       <Route path="/reward" component={Reward} />
       <Route component={NotFound} />
